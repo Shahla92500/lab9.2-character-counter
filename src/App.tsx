6b916d34489c522b4b7edbb6b12cbeac9648333b
Shahla <roksan78@yahoo.com>
2025-11-03 22:35:34 -0800

    code pretty adjustment

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,10 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-50 p-6">
       <div className="max-w-3xl mx-auto">
-      <h1 className="text-green-600 text-xl font-semibold mb-4"> Character Counter App</h1>
-
-    <CharacterCounter minWords={10} maxWords={20} targetReadingTime={2}/>
-    <div style={{marginTop:8, fontSize:14, color:"#888"}}>
-    
-    </div>
-    </div>
+        <h1 className="text-green-600 text-xl font-semibold mb-4"> Character Counter App</h1>
+        <CharacterCounter minWords={10} maxWords={20} targetReadingTime={2}/>
+        <div style={{marginTop:8, fontSize:14, color:"#888"}}>  </div>
+      </div>
     </div>
   )
 }
